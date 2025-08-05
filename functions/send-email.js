95007f36ce458b33248130cb6480d42271e71d4a
Jonathan <jmpiltz@gmail.com>
2025-08-05 12:49:40 -0400

    first commit

---
+++ functions/send-email.js
@@ -0,0 +1,61 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config(); // Only works locally, not on Netlify
+
+exports.handler = async function (event, context) {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ error: 'Method Not Allowed' }),
+    };
+  }
+
+  const { to, subject, text } = JSON.parse(event.body || '{}');
+
+  if (!to) {
+        return res.status(400).json({ error: 'Missing To field' });
+    }
+
+    if (!subject) {
+        return res.status(400).json({ error: 'Missing Subject field' });
+    }
+
+    if (!text) {
+        return res.status(400).json({ error: 'Missing Text field' });
+    }
+  
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
+    },
+});
+
+  const mailOptions = {
+    from: process.env.GMAIL_USER,
+    to,
+    subject,
+    text,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Email sent', id: info.messageId }),
+    };
+  } 
+  
+  catch (err) {
+    console.error('Error sending email:', err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to send email' }),
+    };
+  }
+};
